25d2abe2950a3b9a9d7a9d5fff83f62147ee27ff
Milly Liu <qqmilly33@yahoo.com.tw>
2020-05-27 14:32:58 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+
 const app = express()
 const PORT = 3000
 
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
 app.get('/', (req, res) => {
-  res.send('This is URL shortener web')
+  res.render('index')
 })
 
 app.listen(PORT, () => {
