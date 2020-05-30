18d6d3f734cd43e71d5f38b46323c1bf8acbb379
Milly Liu <qqmilly33@yahoo.com.tw>
2020-05-30 21:40:15 +0800

    refactor: modify express routes

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
 const URL = require('./models/URLshorten')
-const generateURL = require('./generate_url')
 const bodyParser = require('body-parser')
-const indexUrl = 'http://localhost:3000'
+const routes = require('./routes')
 
 const app = express()
 const PORT = 3000
@@ -22,42 +21,9 @@ db.once = ('open', () => {
 
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(routes)
 
-app.get('/', (req, res) => {
-  res.render('index')
-})
-//產生短網址
-app.post('/', (req, res) => {
-  const url = req.body.url
-  //若使用者沒有輸入內容，就按下了送出鈕，需要防止表單送出並提示使用者
-  if (!url.includes('https://') && !url.includes('http://')) {
-    const alert = 'Please input efective URL'
-    res.render('index', { alert, url })
-  }
-  else {
-    let shortUrl = generateURL()
-    return URL.create({
-      originalUrl: url,
-      shortenUrl: shortUrl
-    })
-      .then(() => {
-        res.render('urlshorter', { shortUrl, indexUrl })
-      })
-      .catch(error => console.log(error))
-  }
 
-})
-// 點選短網址連結到原始網站
-app.get('/:newurl', (req, res) => {
-  const { newurl } = req.params
-  //find()第一個參數定義要找尋的資料，第二個參數決定要返回的key；搜尋shortenUrl = newurl者的originalUrl
-  // URL.find({ shortenUrl: newurl }, { originalUrl: 1 })  
-
-  URL.findOne({ shortenUrl: newurl })
-    .lean()
-    .then(url => res.redirect(url.originalUrl))
-    .catch(error => console.log(error))
-})
 
 app.listen(PORT, () => {
   console.log(`Express is running on http://localhost:${PORT}`)
---
+++ routes/modules/home.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const router = express.Router()
+const URL = require('../../models/URLshorten')
+const generateURL = require('../../generate_url')
+const indexUrl = 'http://localhost:3000'
+
+router.get('/', (req, res) => {
+  res.render('index')
+})
+//產生短網址
+router.post('/', (req, res) => {
+  const url = req.body.url
+  //若使用者沒有輸入有效內容，就按下了送出鈕，需要防止表單送出並提示使用者
+  if (!url.includes('https://') && !url.includes('http://')) {
+    const alert = 'Please input efective URL'
+    res.render('index', { alert, url })
+  }
+  else {
+    let shortUrl = generateURL()
+    return URL.create({
+      originalUrl: url,
+      shortenUrl: shortUrl
+    })
+      .then(() => {
+        res.render('urlshorter', { shortUrl, indexUrl })
+      })
+      .catch(error => console.log(error))
+  }
+
+})
+// 點選短網址連結到原始網站
+router.get('/:newurl', (req, res) => {
+  const { newurl } = req.params
+  // console.log(req.params)
+  URL.findOne({ shortenUrl: newurl })
+    .lean()
+    .then(url => res.redirect(url.originalUrl))
+    .catch(error => console.log(error))
+})
+
+module.exports = router
