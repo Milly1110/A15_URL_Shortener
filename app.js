ad10dd8a00af192db74bba9b9aacc65d1ebc6e6a
Milly Liu <qqmilly33@yahoo.com.tw>
2020-05-30 21:51:32 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,23 +1,16 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-const mongoose = require('mongoose')
 const URL = require('./models/URLshorten')
 const bodyParser = require('body-parser')
 const routes = require('./routes')
+require('./config/mongoose')
 
 const app = express()
 const PORT = 3000
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
-mongoose.connect('mongodb://localhost/url_shortener', { useNewUrlParser: true, useUnifiedTopology: true })
-const db = mongoose.connection
-db.on = ('error', () => {
-  console.log('mongodb error!')
-})
-db.once = ('open', () => {
-  console.log('mongodb connected!')
-})
+
 
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
--- models/seeds/urlSeeder.js
+++ models/seeds/urlSeeder.js
@@ -1,10 +1,6 @@
-const mongoose = require('mongoose')
 const URL = require('../URLshorten')
-mongoose.connect('mongodb://localhost/url_shortener', { useNewUrlParser: true, useUnifiedTopology: true })
-const db = mongoose.connection
-db.on('error', () => {
-  console.log(error)
-})
+const db = require('../../config/mongoose')
+
 db.once('open', () => {
   console.log('mongodb connected!')
   URL.create(
